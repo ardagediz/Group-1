84f99ecc436a486540fac7d3d33af20a7b980d39
Arda Gediz <144790644+ardagediz@users.noreply.github.com>
2023-10-19 09:12:41 +0000

    adrA

--- week_3_bootcamp/strlen.c
+++ week_3_bootcamp/strlen.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
-char str_len(char string)
+#include <string.h>
+
+int str_len(char string[])
 {
-    
+    return strlen(string);
+}
+
+int main()
+{
+    char entry[100]; // assuming a max length of 99 characters + 1 for null terminator
+
+    printf("Enter your string here: ");
+    scanf("%99s", entry); // Read up to 99 characters to avoid overflow
+
+    printf("Your string length is %d", str_len(entry));
+
+    return 0;
 }
