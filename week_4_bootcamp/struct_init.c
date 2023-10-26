931b35dfca2e26a480514ac05391a3aee2982477
Arda Gediz <144790644+ardagediz@users.noreply.github.com>
2023-10-26 09:50:21 +0000

    Arda

---
+++ week_4_bootcamp/struct_init.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+struct student {
+    char name [20];
+    char student_id [11]; // Assuming 10 digits.
+    unsigned mark;
+};
+
+int main () {
+    struct student new_student = {"Arda Gediz", "28932123", 99};
+    printf("Student name: %s\n", new_student.name);
+    printf("Student ID:   %s\n", new_student.student_id);
+    printf("Final mark:   %d\n", new_student.mark);
+    return 0;
+}
