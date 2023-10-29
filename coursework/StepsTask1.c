5f756004d351d1ce1e187783755e1b349f8d5c38
Arda Gediz <144790644+ardagediz@users.noreply.github.com>
2023-10-29 11:24:14 +0000

    Changes

--- coursework/StepsTask1.c
+++ coursework/StepsTask1.c
@@ -29,41 +29,25 @@ int main() {
         return 1;
     }
 
-    // Declare an array of char (string) to store each line from the file
 char line[100];
-
-// Declare an integer variable to count the number of records read
 int record_count = 0;
-
-// Declare an array of FITNESS_DATA structures to store the data
 FITNESS_DATA data[1000];
 
-// Loop through each line in the file until the end of the file is reached
 while (fgets(line, sizeof(line), file)) {
-    // Remove the newline character at the end of each line
     line[strcspn(line, "\n")] = 0;
-    // Declare variables to store date, time, and steps after tokenizing the line
-    char date[11], time[6], steps[5];
-    // Call a function to tokenize the line and populate date, time, and steps
-    tokeniseRecord(line, ",", date, time, steps);
-    // Copying the date, time and steps into the correct field of the current record in the 'data' array
-    strcpy(data[record_count].date, date);
-    strcpy(data[record_count].time, time);
-    data[record_count].steps = atoi(steps);
-
-    // Increment the record count
+        
+    tokeniseRecord(line, ",", data[record_count].date, data[record_count].time, line);
+    data[record_count].steps = atoi(line);
+        
     record_count++;
 }
 
-    fclose(file);
+fclose(file);
+printf("Number of records in file: %d", record_count);
 
-    // Outputting the number of records
-    printf("Number of records in file: %d\n", record_count);
+for (int i = 0; i < 3; i++) {
+    printf("\n%s/%s/%d", data[i].date, data[i].time, data[i].steps);
+}
 
-    // Outputting the first three records without extra spaces or newlines
-    for (int i = 0; i < 3; i++) {
-        printf("\n%s/%s/%d\n", data[i].date, data[i].time, data[i].steps);
-    }
-
-    return 0;
+return 0;
 }
