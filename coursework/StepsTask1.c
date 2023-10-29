1838060bd0c6333cde6bad38f83c2f88152fea80
Arda Gediz <144790644+ardagediz@users.noreply.github.com>
2023-10-29 11:29:37 +0000

    Commenting

--- coursework/StepsTask1.c
+++ coursework/StepsTask1.c
@@ -23,28 +23,40 @@ void tokeniseRecord(const char *input, const char *delimiter,
 }
 
 int main() {
+    // Open the fitness data CSV file
     FILE *file = fopen("FitnessData_2023.csv", "r");
     if (file == NULL) {
         perror("Failed\n");
         return 1;
     }
 
+
+// Declare variables for line reading and record counting
 char line[100];
 int record_count = 0;
+// Declare an array to store the fitness data
 FITNESS_DATA data[1000];
 
+// Read and tokenize each line in the file
 while (fgets(line, sizeof(line), file)) {
+    // Remove the newline character from the end of the line
     line[strcspn(line, "\n")] = 0;
         
+    // Tokenize the line and populate the data array
     tokeniseRecord(line, ",", data[record_count].date, data[record_count].time, line);
     data[record_count].steps = atoi(line);
         
+    // Increment the record counter
     record_count++;
 }
 
+// Close the file
 fclose(file);
+
+// Output the number of records read from the file
 printf("Number of records in file: %d", record_count);
 
+// Output the first three records in the required format
 for (int i = 0; i < 3; i++) {
     printf("\n%s/%s/%d", data[i].date, data[i].time, data[i].steps);
 }
